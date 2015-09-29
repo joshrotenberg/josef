62b974d64e85ad5a71b3bac7c6bdcf2acf954c1d
josh rotenberg <joshrotenberg@gmail.com>
2015-09-29 14:55:34 -0700

    add min-lein-version and scm to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,9 @@
   :url "https://github.com/joshrotenberg/josef"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/joshrotenberg/josef"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.apache.kafka/kafka_2.10 "0.8.2.2"]]
   :exclusions [javax.jms/jms
